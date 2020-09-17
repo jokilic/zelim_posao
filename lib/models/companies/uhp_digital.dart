bcb75f9583a34358bea7845cfad8bae72c3922a2
Josip K <neksuses@gmail.com>
2020-09-18 01:18:14 +0200

    Closed #6

--- lib/models/companies/uhp_digital.dart
+++ lib/models/companies/uhp_digital.dart
@@ -8,10 +8,23 @@ Future<Map> getJobsUhpDigital() async {
   var rawUhpDigital = await Network(uhpDigitalURL).getData();
   var parsedUhpDigital = parse(rawUhpDigital);
 
-  List parsedJobs = parsedUhpDigital.getElementsByTagName('h3');
+  List jobSection =
+      parsedUhpDigital.getElementsByClassName('elementor-element-49859ed1');
+  List hiddenJobs =
+      jobSection[0].getElementsByClassName('elementor-hidden-desktop');
 
+  hiddenJobs.forEach((element) {
+    element.remove();
+  });
+
+  List properJobsSection = jobSection[0].getElementsByTagName('section');
+  List properJobs;
   List<String> jobs = [];
-  parsedJobs.forEach((job) => jobs.add(job.text.trim()));
+
+  properJobsSection.forEach((job) {
+    properJobs = job.getElementsByTagName('h3');
+    jobs.add(properJobs[0].text.trim());
+  });
 
   Map finalJobs = {
     'company': uhpDigital,
