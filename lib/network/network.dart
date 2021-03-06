a966fb31004b5184fc13064fbfcc1f706adf9577
jokilic <neksuses@gmail.com>
2021-03-06 15:30:59 +0100

    init Flutter Web

--- lib/network/network.dart
+++ lib/network/network.dart
@@ -7,7 +7,7 @@ class Network {
   Network(this.url);
 
   Future getData() async {
-    http.Response response = await http.get(url);
+    http.Response response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200)
       return response.body;
